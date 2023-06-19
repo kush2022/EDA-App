235b4bd9f3da5b5510e2adac605418c926598df1
kush2022 <felixkuria12@gmail.com>
2023-06-19 15:58:19 +0300

    changing the pandas profiling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 import streamlit as st 
 from streamlit_pandas_profiling import st_profile_report
-from ydata_profiling import ProfileReport
+from pandas_profiling import ProfileReport
 import seaborn as sns 
 from streamlit_lottie import st_lottie
 import requests
